40f7596f5a9bc06863e077b5ca091a57c11b3d93
limac <limac@192.168.1.155>
2017-07-01 13:23:13 +0900

    Without Insert day of the week

--- src/jangre/calendar/Calendar.java
+++ src/jangre/calendar/Calendar.java
@@ -20,11 +20,14 @@ public class Calendar {
 		}
 	}
 
-	public void printCalendar(int year, int month, int weekday) {
-		System.out.printf("    <<%4d년 %3d월>>\n", year, month);
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%d년 %d월>>\n", year, month);
 		System.out.println("  일 월  화  수 목  금 토");
 		System.out.println(" --------------------");
 
+		// get weekday automatically
+		int weekday = getWeekday(year, month, 1);
+		// print blank space
 		for (int i = 0; i < weekday; i++) {
 			System.out.print("   ");
 		}
@@ -32,20 +35,19 @@ public class Calendar {
 		int count = 7 - weekday;
 		int delim = (count < 7) ? count : 0;
 		/*
-		if (count < 7) {
-			delim = count;
-		}
-		else {
-			delim = 0;
-		}
-		*/
-		
-		for(int i = 1; i <= count; i++) {
+		 * if (count < 7) { delim = count; } else { delim = 0; }
+		 */
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
 
-		for (int i = count + 1; i <= maxDay; i++) {
+		// print from second lint to last
+
+		count++;
+		for (int i = count; i <= maxDay; i++) {
 			System.out.printf("%3d", i);
 			if (i % 7 == delim) {
 				System.out.println();
@@ -53,5 +55,29 @@ public class Calendar {
 		}
 		System.out.println();
 		System.out.println();
+	}
+
+	private int getWeekday(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3; // 1970.01.01 = Thursday
+
+		int count = 0;
+
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+
+		// System.out.println(count)
+		for (int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+
+		count += day;
+
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+
+		return weekday;
 	}
 }
--- src/jangre/calendar/Prompt.java
+++ src/jangre/calendar/Prompt.java
@@ -3,25 +3,6 @@ package jangre.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
-	public int parseDay(String week) {
-		if (week.equals("su"))
-			return 0;
-		else if (week.equals("mo"))
-			return 1;
-		else if (week.equals("tu"))
-			return 2;
-		else if (week.equals("wd"))
-			return 3;
-		else if (week.equals("th"))
-			return 4;
-		else if (week.equals("fr"))
-			return 5;
-		else if (week.equals("sa"))
-			return 6;
-		else
-			return 0;
-	}
 
 	public void runPrompt() {
 
@@ -30,7 +11,6 @@ public class Prompt {
 
 		int month = 1;
 		int year = 2017;
-		int weekday = 0;
 
 		while (true) {
 			System.out.println("년도를 입력하세요.(exit : -1)");
@@ -38,20 +18,17 @@ public class Prompt {
 			year = scanner.nextInt();
 			if (year == -1)
 				break;
-			
+
 			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, wd, th, fr, sa)");
-			String str_weekday = scanner.next();
-			weekday = parseDay(str_weekday);
-		
+
 			if (month > 12 || month < 1) {
 				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
-			
-			cal.printCalendar(year, month, weekday);
+
+			cal.printCalendar(year, month);
 		}
 
 		System.out.println("Bye~~");
